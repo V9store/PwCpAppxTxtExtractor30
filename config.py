cd557dbec24beff5a2836f14c84bfcad8e402092
V9store <v9store41@gmail.com>
2025-07-31 07:45:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "21283957"
+api_hash = "aade44a828de52da2a6ef816b120020b"
+bot_token = "8298926471:AAGWYeyvVMHVjBfkWT5z_HuhVx-q-Hn1mhM"
+auth_users = [6917342289]
