aaa1cb7d38769502efa958f37445a8dd9b6ebf99
V9store <v9store41@gmail.com>
2025-07-31 08:42:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 
 # Bot credentials from environment variables (Render compatible)
-API_ID = int(os.environ.get("API_ID", 24473318))
-API_HASH = os.environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0")
+API_ID = int(os.environ.get("API_ID", 21283957))
+API_HASH = os.environ.get("API_HASH", "aade44a828de52da2a6ef816b120020b")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8298926471:AAGWYeyvVMHVjBfkWT5z_HuhVx-q-Hn1mhM")
 
 # Initialize Bot Globally (IMPORTANT FIX)
 bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
